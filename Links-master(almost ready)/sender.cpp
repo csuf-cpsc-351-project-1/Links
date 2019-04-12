0d5f73ebd7176db562d6ddc2c29a1f5a3c7010ef
yanjieshi <47162723+yanjieshi@users.noreply.github.com>
2019-04-12 15:05:33 -0700

    Add files via upload

--- Links-master(almost ready)/sender.cpp
+++ Links-master(almost ready)/sender.cpp
@@ -5,6 +5,10 @@
 #include <unistd.h>
 #include "msg.h"    /* For the message struct */
 
+// added includes
+// #include <sys/ipc.h>
+#include <iostream>
+
 /* The size of the shared memory chunk */
 #define SHARED_MEMORY_CHUNK_SIZE 1000
 
@@ -22,12 +26,18 @@ void* sharedMemPtr;
 
 void init(int& shmid, int& msqid, void*& sharedMemPtr)
 {
-	
+
+	// create key
 	key_t bathroomkey;
 
-	
+	// assign key
+	if ((bathroomkey = ftok("keyfile.txt", 'a')) == -1) {
+		std::cout << "An error occurred while creating the key in sender.\n";
+		exit(-1);
+	}
 
-    bathroomkey = ftok("keyfile.txt", 'a');
+	// track the key for debugging purposes
+	std::cout << "The key is: " << bathroomkey << std::endl;
 
 	/*
 	NOTE:	Use the key in the TODO's below. Use the same key for the queue
@@ -38,12 +48,27 @@ void init(int& shmid, int& msqid, void*& sharedMemPtr)
 	may have the same key.
 	 */
 
-shmid = shmget(bathroomkey, SHARED_MEMORY_CHUNK_SIZE, 0666 | IPC_CREAT);
+	// find the shared memory id
+	if ((shmid = shmget(bathroomkey, SHARED_MEMORY_CHUNK_SIZE, 0644 | IPC_CREAT)) == -1) {
+		std::cout << "An error occurred while finding the shared memory id.\n";
+		exit(-1);
+	}
 
+	// attach to the shared memory
+	sharedMemPtr = shmat(shmid, (void *)NULL, 0);
+	if (sharedMemPtr == (char*)(-1)) {
+		std::cout << "An error occurred while attempting to attach to the shared memory.\n";
+		exit(-1);
+	}
 
-sharedMemPtr = shmat(shmid, NULL, 0);
+	// attach to message queue
+	if ((msqid = msgget(bathroomkey, 0644 | IPC_CREAT)) == -1) {
+		std::cout << "An error occurred while attaching to the message queue.\n";
+		exit(-1);
+	}
 
-msqid = msgget(bathroomkey, 0666 | IPC_CREAT);
+	std::cout << "The message queue id is: " << msqid << std::endl;
+	std::cout << "The shared memory id is: " << shmid << std::endl;
 
 }
 
@@ -57,7 +82,13 @@ msqid = msgget(bathroomkey, 0666 | IPC_CREAT);
 void cleanUp(const int& shmid, const int& msqid, void* sharedMemPtr)
 {
 	/* TODO: Detach from shared memory */
-	shmdt(sharedMemPtr);
+	if (shmdt(sharedMemPtr) == 0) {
+		std::cout << "Successfully detached shared memory pointer.\n";
+	}
+	else {
+		std::cout << "An error occurred while attempting to detach the shared memory pointer.\n";
+		exit(-1);
+	}
 }
 
 /**
@@ -71,60 +102,79 @@ void send(const char* fileName)
 
 
 	/* A buffer to store message we will send to the receiver. */
-	message sndMsg;
+	message sendMsg;
 
 	/* A buffer to store message received from the receiver. */
-	message rcvMsg;
+	message recvMsg;
 
 	/* Was the file open? */
-	if(!fp)
+	if (!fp)
 	{
-		perror("fopen");
+		std::cout << "The file was not open.\n";
 		exit(-1);
 	}
 
 	/* Read the whole file */
-	while(!feof(fp))
+	while (!feof(fp))
 	{
 		/* Read at most SHARED_MEMORY_CHUNK_SIZE from the file and store them in shared memory.
- 		 * fread will return how many bytes it has actually read (since the last chunk may be less
- 		 * than SHARED_MEMORY_CHUNK_SIZE).
- 		 */
-		if((sndMsg.size = fread(sharedMemPtr, sizeof(char), SHARED_MEMORY_CHUNK_SIZE, fp)) < 0)
+		 * fread will return how many bytes it has actually read (since the last chunk may be less
+		 * than SHARED_MEMORY_CHUNK_SIZE).
+		 */
+		if ((sendMsg.size = fread(sharedMemPtr, sizeof(char), SHARED_MEMORY_CHUNK_SIZE, fp)) < 0)
 		{
-			perror("fread");
+			std::cout << "An error occurred when reading from file.\n";
 			exit(-1);
 		}
 
 
 		/* TODO: Send a message to the receiver telling him that the data is ready
- 		 * (message of type SENDER_DATA_TYPE)
- 		 */
-		
-		 sndMsg.mtype = SENDER_DATA_TYPE;
+		 * (message of type SENDER_DATA_TYPE)
+		 */
 
-		 msgsnd(msqid, &sndMsg, sizeof(message), 0);
+		sendMsg.mtype = SENDER_DATA_TYPE;
+
+		if ((msgsnd(msqid, &sendMsg, sizeof(message), 0)) < 0) {
+			std::cout << "The message was unable to send.\n";
+			exit(-1);
+		}
+
+		std::cout << "The message sent has a chunk size of: " << sendMsg.size << std::endl;
 
 
 		/* TODO: Wait until the receiver sends us a message of type RECV_DONE_TYPE telling us
- 		 * that he finished saving the memory chunk.
+		 * that he finished saving the memory chunk.
 
- 		 */
-				
-		msgrcv(msqid, &rcvMsg, sizeof(message), RECV_DONE_TYPE,0);
-			 
+		 */
+
+		std::cout << "Waiting for response.\n";
+
+		if ((msgrcv(msqid, &recvMsg, sizeof(message), RECV_DONE_TYPE, 0)) < 0) {
+			std::cout << "No response from receiver. Exiting...\n";
+			exit(-1);
+		}
+
+		std::cout << "Response received.\n";
+
 	}
 
 
 	/** TODO: once we are out of the above loop, we have finished sending the file.
- 	  * Lets tell the receiver that we have nothing more to send. We will do this by
- 	  * sending a message of type SENDER_DATA_TYPE with size field set to 0.
+	  * Lets tell the receiver that we have nothing more to send. We will do this by
+	  * sending a message of type SENDER_DATA_TYPE with size field set to 0.
 	  */
-	  sndMsg.size = 0;
-	  msgsnd(msqid, &sndMsg, 0, 0);
+	sendMsg.mtype = SENDER_DATA_TYPE;
+	sendMsg.size = 0;
+	if ((msgsnd(msqid, &sendMsg, 0, 0)) < 0) {
+		std::cout << "Failed to send the final message to the receiver.\n";
+		exit(-1);
+	}
 
 	/* Close the file */
-	fclose(fp);
+	if (fclose(fp) != 0) {
+		std::cout << "Error occurred when attempting to close the file.\n";
+		exit(-1);
+	}
 
 }
 
@@ -133,7 +183,7 @@ int main(int argc, char** argv)
 {
 
 	/* Check the command line arguments */
-	if(argc < 2)
+	if (argc < 2)
 	{
 		fprintf(stderr, "USAGE: %s <FILE NAME>\n", argv[0]);
 		exit(-1);
